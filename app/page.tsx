76325fd1babbdf7351db6ad8b1ec4890788db54d
Sergey <serzh.kachalyuk@gmail.com>
2024-03-15 13:42:26 +0300

    Replaced apostrophies with valid HTML replacers

--- app/page.tsx
+++ app/page.tsx
@@ -2,112 +2,67 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="min-h-screen p-16">
+      <div className="max-w-[900px] ml-auto mr-auto">
+
+        <section className="flex justify-center">
+          <div className="mr-10 flex flex-col justify-between max-w-[600px]">
+            <h1 className="font-bold text-3xl">Sergey Kachaliuk</h1>
+            <h2 className="mt-2">Front-End Engineer</h2>
+            <p className="mt-6 font-bold mb-3">Hello, I&apos;m glad to see you here!</p>
+            <p className="mt-0">My name is Sergey, I am a Front-End Engineer and I&apos;m passionate about creating beautiful, creative and inspiring websites &amp; applications that help people and businesses reach their goals and make our life easier.</p>
+            <p className="mt-5 mb-3">To find out more about me and my skills, please check out my social media:</p>
+            <div className="flex">
+              <a href="https://github.com/17Sergey" title="GitHub" target="_blank">
+                <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub" className="w-8 mr-2" />
+              </a>
+              <a href="https://www.linkedin.com/in/sergey-kachaliuk-3a5b762b6/" title="LinkedIn" target="_blank">
+                <img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/circle-linkedin-512.png" alt="LinkedIn" className="w-8" />
+              </a>
+            </div>
+          </div>
+          <img src="./me.jpg" alt="" className="w-[300px]" />
+        </section>
+
+        <section className="mt-20">
+          <h3 className="font-bold text-2xl">Projects:</h3>
+
+          <ul className="mt-10 flex-col">
+            <li className="text-left px-6 py-4 border-2 border-cyan-300">
+              <p className="font-bold text-lg ">ChashaChurch freelance project</p>
+              <p className="mt-5 max-w-[400px]">Website for a church organization was created to make it possible for people of different ages to simply use the website and get the needed information. The website is still in use and growing with more functionality.</p>
+              <a
+                className="inline-block text-sm mt-5 px-4 py-1.5 bg-cyan-300 rounded-2xl"
+                target="_blank"
+                href="https://chasha.netlify.app/"
+                title="ChashaChurch"
+              >Open website</a>
+            </li>
+            <li className="my-4 text-left px-6 py-4 border-2 border-cyan-300">
+              <p className="font-bold text-lg ">Furious World university project</p>
+              <p className="mt-5 max-w-[400px]">Website for fans of the Fast&amp;Furious movies franchise. It was created using React, Redux and TypeScript.</p>
+              <a
+                className="inline-block text-sm mt-5 px-4 py-1.5 bg-cyan-300 rounded-2xl"
+                target="_blank"
+                href="https://furiousworld.netlify.app/"
+                title="Furious World"
+              >Open website</a>
+            </li>
+            <li className="text-left px-6 py-4 border-2 border-cyan-300">
+              <p className="font-bold text-lg ">Alpha Paintball Club university project</p>
+              <p className="mt-5 max-w-[400px]">Website of a paintball club &apos;Alpha&apos; with lots of interactive interfaces that may wonder the user. It was created with HTML5, CSS3. SCSS. JavaScript, JQuery.</p>
+              <a
+                className="inline-block text-sm mt-5 px-4 py-1.5 bg-cyan-300 rounded-2xl"
+                target="_blank"
+                href="https://alphapaintball.netlify.app/"
+                title="Alpha Paintball Club"
+              >Open website</a>
+            </li>
+          </ul>
+        </section>
+
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
